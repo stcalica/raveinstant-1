7eae4469f09c5c3fb59a7a2d6b80611defcb4853
Clay Han <clayhan1@gmail.com>
2016-12-10 12:58:12 -0800

    Fixed server parts that were lost.

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,13 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const path = require('path');
+
+// This code can be modularized later
+const sendHome = function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../../client/index.html'));
+};
+
+module.exports = {
+  sendHome: sendHome
+};
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const router = require('./routes');
 
 app.use(express.static(__dirname + '/../client'));
 app.port = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
-  console.log('Sending index file');
-  res.sendFile(path.resolve(__dirname + '/../client/index.html'));
-});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// middleware for routes
+// https://expressjs.com/en/guide/routing.html
+app.use('/', router);
+
+// app.get('*', (req, res) => {
+//   console.log('Sending index file');
+//   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+// });
 
 app.listen(app.port, () => {
   console.log('Listening on port:', app.port);
