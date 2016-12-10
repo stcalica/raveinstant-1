448f4f49110bd42ec3576c66a7d2815199662bfa
Clay Han <clayhan1@gmail.com>
2016-12-10 11:47:30 -0800

    Creating environment

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Uncomment when implementing routes
+// const router = require('./routes');
+
+app.use(express.static(__dirname + '/../client'));
+app.port = process.env.PORT || 3000;
+
+app.get('*', (req, res) => {
+  console.log('Sending index file');
+  res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+
+app.listen(app.port, () => {
+  
+  console.log('Listening on port:', app.port);
+});
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: [
+    __dirname + '/client/src/index.js'
+  ],
+  output: {
+    path: __dirname + '/client',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
